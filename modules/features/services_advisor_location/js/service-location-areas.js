d6f5e87b55b774561e37ea525c41c2146bca1e76
Tom Nightingale <tom@tnightingale.com>
2017-07-25 11:41:09 -0700

    Refs #98 - Not displaying service location polygon on map.

--- modules/features/services_advisor_location/js/service-location-areas.js
+++ modules/features/services_advisor_location/js/service-location-areas.js
@@ -14,13 +14,13 @@
                     var map = Drupal.leaflet_widget[id];
                     var fieldId = '#edit-field-service-location-location';
                     var inputClass = '.hierarchical-select select';
-                    var selectedArea = undefined;
+                    // var selectedArea = undefined;
 
                     $(fieldId).on('change', inputClass, function (e) {
-                        if (selectedArea) {
-                            map.removeLayer(selectedArea);
-                            selectedArea = undefined;
-                        }
+                        // if (selectedArea) {
+                        //     map.removeLayer(selectedArea);
+                        //     selectedArea = undefined;
+                        // }
                         // TODO: We want to use selected values as keys to
                         //       access polygons in our polygon lookup table.
                         //
@@ -38,8 +38,8 @@
                         var tid = parseInt(e.target.value, 10);
                         var area = serviceAreas[tid]
                         if (area && area.features && area.features.length) {
-                            selectedArea = L.geoJson(area);
-                            selectedArea.addTo(map);
+                            // selectedArea = L.geoJson(area);
+                            // selectedArea.addTo(map);
 
                             var bounds = L.latLngBounds([]);
                             var latLngs = area.features.forEach(function (feature) {
